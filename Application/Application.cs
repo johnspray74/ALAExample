a52debac2abbf4e16914b8b648837efa0cf29ecc
JohnS <john.spray@trutest.co.nz>
2021-06-21 17:18:16 +1200

    More comments for first time readers

--- Application/Application.cs
+++ Application/Application.cs
@@ -8,7 +8,8 @@ namespace Application
 {
     public class Application
     {
-        // Application is an abstraction in the top most concrete layer of ALA (Abstraction Layered Architecture)
+        // Application.cs is code hand-generated from Application-diagram in the same folder. They are the real source code.
+        // This class is an abstraction in the top most concrete layer of ALA (Abstraction Layered Architecture)
         // (for bmore background refer https://www.github.com/johnspray74/ALAExample http://www.abstractionlayeredarchitecture.com)
         // Actually the diagram (application-diagram in the same folder) is the source code - read that instead.
         // This Application.cs class is just hand compiled wiring code following that diagram.
@@ -37,7 +38,41 @@ namespace Application
 
         private Application()
         {
-            // First instantiate any domain abstractions that we can't leave anonymous because the wiring diagram has loops in it.
+            // This is a real farming device simulator abstraction that we configure with mock data.
+            // This simulated device gets wired to the COMPort abstraction but is not itself part of the application. 
+            // In the real application, this would be a real farming device connected to the physical COM port of the PC.
+            // to receive command and return data.
+            var scpSimulator = new RealFarmingDeviceSimulator();
+
+            // Configure the simulated device with soem data
+            scpSimulator.AddSession(name: "session0", date: "2021-02-28", new[] { "F01FID", "F11EID" });
+            scpSimulator.AddSession("session1", "2021-03-31", new[] { "F01FID", "F11EID", "F10Weight" });
+            scpSimulator.AddSession("session2", "2021-04-30", new[] { "F01FID", "F11EID", "F10Weight", "F12Remark" });
+            scpSimulator.AddSessionData(0, new[] { "1", "EID0000000000000" });
+            scpSimulator.AddSessionData(0, new[] { "2", "EID0000000000001" });
+            scpSimulator.AddSessionData(1, new[] { "013", "EID0000000000010", "342" });
+            scpSimulator.AddSessionData(1, new[] { "001", "EID0000000000011", "373" });
+            scpSimulator.AddSessionData(1, new[] { "002", "EID0000000000012", "304" });
+            scpSimulator.AddSessionData(2, new[] { "0123", "EID0000000000021", "405", "healthy" });
+            scpSimulator.AddSessionData(2, new[] { "1023", "EID0000000000022", "376", "pregnant" });
+            scpSimulator.AddSessionData(2, new[] { "0412", "EID0000000000023", "354", "black spot" });
+            scpSimulator.AddSessionData(2, new[] { "0219", "EID0000000000024", "395", "lame" });
+
+
+
+
+
+            // -------------------------- CODE MANUALLY GENERATED FROM DIAGRAM --------------------------------------------------------------
+            // The following code has been manually generated from the diagram in application-diagram.drawio, also in application-diagram.pdf
+            // Refer to the diagram for how the application works, not this code.
+            // Also application.md is a commentary on reading the diagram.
+            // Take an interest in this code if
+            // 1. You want to understand how the diagram was hand-coded
+            // 2. You want to know how the ALA diagram was made to actually execute
+            // 3. You have modified the diagram and need to correspondingly modify this code
+
+
+            // First instantiate any domain abstractions that we can't leave anonymous because the wiring diagram has loops in it:
 
             var saveFileBrowser = new SaveFileBrowser("Save file", "CSV");
             var textConnected = new Text("Connected") { Color = Brushes.Green };
@@ -54,31 +89,12 @@ namespace Application
             var csvFileReaderWriter = new CSVFileReaderWriter();
 
 
-
-
-            // This is a simulator abstraction that we configure with mock data.
-            // This simulated device is wired to the COMPort abstraction but is not itself part of the application. 
-            // In the real application, this would be a real farming device connected to the physical COM port of the PC.
-            // to receive command and return data.
-            var scpSimulator = new RealFarmingDeviceSimulator();
-
-            // Configure the simulated device with soem data
-            scpSimulator.AddSession(name:"session0", date:"2021-02-28", new[] { "F01FID","F11EID" });
-            scpSimulator.AddSession("session1", "2021-03-31", new[] { "F01FID", "F11EID", "F10Weight" });
-            scpSimulator.AddSession("session2", "2021-04-30", new[] { "F01FID", "F11EID", "F10Weight", "F12Remark" });
-            scpSimulator.AddSessionData(0, new[] { "1", "EID0000000000000" });
-            scpSimulator.AddSessionData(0, new[] { "2", "EID0000000000001" });
-            scpSimulator.AddSessionData(1, new[] { "013", "EID0000000000010", "342" });
-            scpSimulator.AddSessionData(1, new[] { "001", "EID0000000000011", "373" }); 
-            scpSimulator.AddSessionData(1, new[] { "002", "EID0000000000012", "304" });
-            scpSimulator.AddSessionData(2, new[] { "0123", "EID0000000000021", "405", "healthy" }); 
-            scpSimulator.AddSessionData(2, new[] { "1023", "EID0000000000022", "376", "pregnant" });
-            scpSimulator.AddSessionData(2, new[] { "0412", "EID0000000000023", "354", "black spot" });
-            scpSimulator.AddSessionData(2, new[] { "0219", "EID0000000000024", "395", "lame" });
-
-
-
-
+            // Now do all the wiring of the diagram
+            // Note any instances of domain abstractions not already instantiated above are anonymous in this code.
+            // Note that a.wireTo(b) is an extension method that uses reflection to wire 'a' to 'b' via 'ports'. The ports on 'a' and 'b' must be of the same interface type. 'a' must have a private field of the interface and 'b' must implement the interface.
+            // Note that the fluent style is used: wireTo returns its first argument, allowing you to call wireTo again to wire in another instance of a domain abstraction.
+            // Note that InstanceName properties are not needed by the code - they are just to help when debugging becasue they give instances of the same domain abstraction in different places different names.
+            // Sometime wireTo has a second parameter which is the name of the specific port it is wiring to.
             mainWindow
             // UI
             .WireTo(new Vertical(true) { Layouts = new int[] { 0, 0, 2, 0 } }
